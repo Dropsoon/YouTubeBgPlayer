9ce24a45ab7879d60e4661afb328f9d3b7903ba5
Dropsoon <robertwejman4@gmail.com>
2021-04-09 14:42:46 +0200

    Added functionality of playing music and modern GUI

--- YouTubeBgPlayer/Form1.Designer.cs
+++ YouTubeBgPlayer/Form1.Designer.cs
@@ -29,20 +29,92 @@ namespace YouTubeBgPlayer
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.flowControls = new System.Windows.Forms.FlowLayoutPanel();
+            this.bNext = new System.Windows.Forms.Button();
+            this.bPrevious = new System.Windows.Forms.Button();
+            this.flowControls.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(2, 2);
+            this.buttonPlay.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(70, 29);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(76, 2);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(66, 29);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // flowControls
+            // 
+            this.flowControls.Controls.Add(this.buttonPlay);
+            this.flowControls.Controls.Add(this.buttonStop);
+            this.flowControls.Controls.Add(this.bNext);
+            this.flowControls.Controls.Add(this.bPrevious);
+            this.flowControls.Location = new System.Drawing.Point(16, 62);
+            this.flowControls.Margin = new System.Windows.Forms.Padding(2);
+            this.flowControls.Name = "flowControls";
+            this.flowControls.Size = new System.Drawing.Size(286, 35);
+            this.flowControls.TabIndex = 2;
+            // 
+            // bNext
+            // 
+            this.bNext.Location = new System.Drawing.Point(146, 2);
+            this.bNext.Margin = new System.Windows.Forms.Padding(2);
+            this.bNext.Name = "bNext";
+            this.bNext.Size = new System.Drawing.Size(66, 29);
+            this.bNext.TabIndex = 2;
+            this.bNext.Text = "Next";
+            this.bNext.UseVisualStyleBackColor = true;
+            this.bNext.Click += new System.EventHandler(this.bNext_Click);
+            // 
+            // bPrevious
+            // 
+            this.bPrevious.Location = new System.Drawing.Point(216, 2);
+            this.bPrevious.Margin = new System.Windows.Forms.Padding(2);
+            this.bPrevious.Name = "bPrevious";
+            this.bPrevious.Size = new System.Drawing.Size(66, 29);
+            this.bPrevious.TabIndex = 3;
+            this.bPrevious.Text = "Pervious";
+            this.bPrevious.UseVisualStyleBackColor = true;
+            this.bPrevious.Click += new System.EventHandler(this.bPrevious_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(545, 337);
+            this.ClientSize = new System.Drawing.Size(382, 253);
+            this.Controls.Add(this.flowControls);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(14, 60, 14, 15);
+            this.Text = "YouTubePlayer";
+            this.flowControls.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.FlowLayoutPanel flowControls;
+        private System.Windows.Forms.Button bNext;
+        private System.Windows.Forms.Button bPrevious;
     }
 }
 
--- YouTubeBgPlayer/Form1.cs
+++ YouTubeBgPlayer/Form1.cs
@@ -7,15 +7,174 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.IO;
+using MetroFramework;
+
+using MetroFramework.Components;
+using MetroFramework.Forms;
 
 namespace YouTubeBgPlayer
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroFramework.Forms.MetroForm
     {
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+
+        string[] qId = new string[1024];
+        string[] qTitle = new string[1024];
+        int addedSongs = 0;
+        int currentSong = 0;
+        bool bStopClicekd = false;
+        bool nextorprev = false;
+        float volume = 0.1f;
         public Form1()
         {
             InitializeComponent();
+            var flowPanel = this.flowControls;
+            flowPanel.FlowDirection = FlowDirection.LeftToRight;
+            flowPanel.Margin = new Padding(10);
+
+            var buttonPlay = this.buttonPlay;
+            buttonPlay.Text = "Play";
+            buttonPlay.Click += buttonPlay_Click;
+            flowPanel.Controls.Add(buttonPlay);
+
+            var buttonStop = this.buttonStop;
+            buttonStop.Text = "Stop";
+            buttonStop.Click += buttonStop_Click;
+            flowPanel.Controls.Add(buttonStop);
+
+            this.Controls.Add(flowPanel);
+
+            this.FormClosing += buttonStop_Click;
+
+            LoadData();
         }
 
+        public void LoadData()
+        {
+            FileStream fs;
+            if (File.Exists("songs/ids.txt"))
+            {
+                fs = File.OpenRead("songs/ids.txt");
+                var sr = new StreamReader(fs);
+                string line;
+                int i = 0;
+                while((line = sr.ReadLine()) != null)
+                {
+                    qId[i++] = line;
+                    addedSongs++;
+                }
+                fs.Close();
+            } else
+            {
+                File.Create("songs/ids.txt");
+            }
+            for(int i=0; i<addedSongs; i++)
+            {
+                string filename = "songs/data/" + qId[i] + ".txt";
+                fs = File.OpenRead(filename);
+                var sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                if(line == qId[i])
+                {
+                    line = sr.ReadLine();
+                    qTitle[i] = line;
+                }
+                fs.Close();
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                if(addedSongs > 0)
+                {
+                    string fileName = "songs/files/" + qId[currentSong] + ".mp3";
+                    audioFile = null;
+                    audioFile = new AudioFileReader(fileName);
+                    outputDevice.Init(audioFile);
+                    audioFile.Volume = volume;
+                }
+                
+            }
+            outputDevice.Play();
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            if(currentSong != addedSongs-1 && !bStopClicekd && !nextorprev)
+            {
+                currentSong++;
+                string fileName = "songs/files/" + qId[currentSong] + ".mp3";
+                audioFile = null;
+                audioFile = new AudioFileReader(fileName);
+                outputDevice.Init(audioFile);
+                audioFile.Volume = volume;
+                outputDevice.Play();
+                bStopClicekd = false;
+            }
+            else if(!nextorprev)
+            {
+                outputDevice.Dispose();
+                outputDevice = null;
+                audioFile.Dispose();
+                audioFile = null;
+                bStopClicekd = false;
+            }
+            else
+            {
+                nextorprev = false;
+            }
+            
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            outputDevice?.Stop();
+            bStopClicekd = true;
+        }
+
+        private void bNext_Click(object sender, EventArgs e)
+        {
+            if(currentSong != addedSongs - 1)
+            {
+                nextorprev = true;
+                outputDevice?.Stop();
+                currentSong++;
+                string fileName = "songs/files/" + qId[currentSong] + ".mp3";
+                audioFile = null;
+                audioFile = new AudioFileReader(fileName);
+                outputDevice.Init(audioFile);
+                audioFile.Volume = volume;
+                outputDevice.Play();
+                bStopClicekd = false;
+            }
+        }
+
+        private void bPrevious_Click(object sender, EventArgs e)
+        {
+            if (currentSong != 0)
+            {
+                nextorprev = true;
+                outputDevice?.Stop();
+                currentSong--;
+                string fileName = "songs/files/" + qId[currentSong] + ".mp3";
+                audioFile = null;
+                audioFile = new AudioFileReader(fileName);
+                outputDevice.Init(audioFile);
+                audioFile.Volume = volume;
+                outputDevice.Play();
+                bStopClicekd = false;
+            }
+        }
     }
 }
